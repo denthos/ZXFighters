60e4c674fb2c386de9ba3f584bb7ea67a5f9ab0d
Paul Wallace <paulrwall@gmail.com>
2017-01-20 16:35:12 -0800

    Wrote a slightly more complex hello world to get a handle on how to find attribute bytes in RAM

--- src/main.asm
+++ src/main.asm
@@ -1,8 +1,8 @@
-	; This is a basic hello world style example that
-	; simply colors the color cell in the top left of
-	; the screen a dark blue color by loading the
-	; byte 0b00001001 into the first attribute byte
-	; in VRAM.
+; This is a basic hello world style example that
+; simply colors the color cell in the top left of
+; the screen a dark blue color by loading the
+; byte 0b00001001 into the first attribute byte
+; in VRAM.
 
 start:	ld	a, 9
 loop:	ld	(0x5800), a
