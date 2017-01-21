461873db4b6a2f92ab9ee8ffa749861a9954790f
Dylan Pereira <dpereira@ucsd.edu>
2017-01-20 17:19:34 -0800

    added byte address calculation subroutines and showed that calling them from main doesn't crash the program

--- src/main.asm
+++ src/main.asm
@@ -4,6 +4,32 @@
 	; byte 0b00001001 into the first attribute byte
 	; in VRAM.
 
-start:	ld	a, 9
-loop:	ld	(0x5800), a
-	jr	loop
+        org 32768
+start:
+        ld a, 2         ; channel 2 = "S" for screen
+        call $1601      ; Select print channel using ROM
+        ld hl,line      ; Print line
+        call printline
+        ld b, 0
+        ld c, 0
+        call calculate_pixel_byte_address
+        ld a, 2         ; channel 2 = "S" for screen
+        call $1601      ; Select print channel using ROM
+        ld hl,line      ; Print line
+        call printline
+        ret
+
+printline:              ; Routine to print out a line
+        ld a,(hl)       ; Get character to print
+        cp '$'          ; See if it '$' terminator
+        jp z,printend   ; We're done if it is
+        rst 16          ; Spectrum: Print the character in 'A'
+        inc hl          ; Move onto the next character
+        jp printline    ; Loop round
+printend:
+        ret
+
+
+line:   defb 'Hello, world!',13,'$'
+
+        include "src/ByteAddressUtils.asm"
