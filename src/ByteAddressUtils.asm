9ef869ea032543368892029e6f5eea469905e55f
Dylan Pereira <dpereira@ucsd.edu>
2017-01-20 17:26:43 -0800

    fixed spacing issues from editor

--- src/ByteAddressUtils.asm
+++ src/ByteAddressUtils.asm
@@ -17,28 +17,28 @@
 ;		HL = address of pixel byte containing pixel
 ; ------------------------------------------------------------------------------
 calculate_pixel_byte_address:
-				ld a, c			; a is now: y7 y6 y5 y4 y3 y2 y1 y0
-				and 0x40			; set base bits
-				ld h, a			; h is now: 0  1  0  y4 y3 y2 y1 y0
-				ld a, c			; a is now: y7 y6 y5 y4 y3 y2 y1 y0
-				srl a
-				srl a
-				srl a
-				and 0x18			; a is now: 0  0  0  y7 y6 0  0  0
-				or h					; a is now: 0  1  0  y7 y6 y2 y1 y0
-				ld h, a			; we are done with h now
-				ld a, b			; a is now: x7 x6 x5 x4 x3 x2 x1 x0
-				srl a
-				srl a
-				srl a					; a is now: 0  0  0  x7 x6 x5 x4 x3
-				ld l, a			; l is now: 0  0  0  x7 x6 x5 x4 x3
-				ld a, c			; a is now: y7 y6 y5 y4 y3 y2 y1 y0
-				sla a
-				sla a
-				and 0xe0			; a is now: y5 y4 y3 0  0  0  0  0
-				or l					; a is now: y5 y4 y3 x7 x6 x5 x4 x3
-				ld l, a			; we are done with l now
-				ret
+		ld a, c     ; a is now: y7 y6 y5 y4 y3 y2 y1 y0
+		and 0x40    ; set base bits
+		ld h, a     ; h is now: 0  1  0  y4 y3 y2 y1 y0
+		ld a, c     ; a is now: y7 y6 y5 y4 y3 y2 y1 y0
+		srl a
+		srl a
+		srl a
+		and 0x18    ; a is now: 0  0  0  y7 y6 0  0  0
+		or h        ; a is now: 0  1  0  y7 y6 y2 y1 y0
+		ld h, a     ; we are done with h now
+		ld a, b     ; a is now: x7 x6 x5 x4 x3 x2 x1 x0
+		srl a
+		srl a
+		srl a       ; a is now: 0  0  0  x7 x6 x5 x4 x3
+		ld l, a     ; l is now: 0  0  0  x7 x6 x5 x4 x3
+		ld a, c     ; a is now: y7 y6 y5 y4 y3 y2 y1 y0
+		sla a
+		sla a
+		and 0xe0    ; a is now: y5 y4 y3 0  0  0  0  0
+		or l        ; a is now: y5 y4 y3 x7 x6 x5 x4 x3
+		ld l, a     ; we are done with l now
+		ret
 
 
 ; ------------------------------------------------------------------------------
@@ -52,27 +52,27 @@ calculate_pixel_byte_address:
 ;		HL = address of attr byte that determines the color of the specified pixel
 ; ------------------------------------------------------------------------------
 calculate_attr_byte_address:
-				ld a, c			; a is now: y7 y6 y5 y4 y3 y2 y1 y0
-				srl a
-				srl a
-				srl a
-				srl a
-				srl a
-				srl a
-				and 0x5b			; a is now: 0  1  0  1  1  0  y7 y6
-				ld h, a			; we are done with h now
-				ld a, c			; a is now: y7 y6 y5 y4 y3 y2 y1 y0
-				sla a
-				sla a
-				and 0xe0			; a is now: y5 y4 y3 0  0  0  0  0
-				ld l, a			; l is now: y5 y4 y3 0  0  0  0  0
-				ld a, b			; a is now: x7 x6 x5 x4 x3 x2 x1 x0
-				srl a
-				srl a
-				srl a					; a is now: 0  0  0  x7 x6 x5 x4 x3
-				or l					; a is now: y5 y4 y3 x7 x6 x5 x4 x3
-				ld l, a			; we are done with l now
-				ret
+		ld a, c     ; a is now: y7 y6 y5 y4 y3 y2 y1 y0
+		srl a
+		srl a
+		srl a
+		srl a
+		srl a
+		srl a
+		and 0x5b    ; a is now: 0  1  0  1  1  0  y7 y6
+		ld h, a     ; we are done with h now
+		ld a, c     ; a is now: y7 y6 y5 y4 y3 y2 y1 y0
+		sla a
+		sla a
+		and 0xe0    ; a is now: y5 y4 y3 0  0  0  0  0
+		ld l, a     ; l is now: y5 y4 y3 0  0  0  0  0
+		ld a, b     ; a is now: x7 x6 x5 x4 x3 x2 x1 x0
+		srl a
+		srl a
+		srl a       ; a is now: 0  0  0  x7 x6 x5 x4 x3
+		or l        ; a is now: y5 y4 y3 x7 x6 x5 x4 x3
+		ld l, a     ; we are done with l now
+		ret
 
 
 ; ------------------------------------------------------------------------------
@@ -86,17 +86,17 @@ calculate_attr_byte_address:
 ;		HL = address of the first pixel byte in the specified color cell
 ; ------------------------------------------------------------------------------
 calculate_color_cell_pixel_address:
-				ld a, c			; a is now: 0  0  0  y7 y6 y5 y4 y3
-				and 0x58			; a is now: 0  1  0  y7 y6 0  0  0
-				ld h, a			; we are done with h now
-				ld a, c			; a is now: 0  0  0  y7 y6 y5 y4 y3
-				and 0x7				; strip y7 and y6
-				rrca
-				rrca
-				rrca					; a is now: y5 y4 y3 0  0  0  0  0
-				add a, b			; a is now: y5 y4 y3 x7 x6 x5 x4 x3
-				ld l, a			; we are done with l now
-				ret
+    ld a, c     ; a is now: 0  0  0  y7 y6 y5 y4 y3
+    and 0x58    ; a is now: 0  1  0  y7 y6 0  0  0
+    ld h, a     ; we are done with h now
+    ld a, c     ; a is now: 0  0  0  y7 y6 y5 y4 y3
+    and 0x7     ; strip y7 and y6
+    rrca
+    rrca
+    rrca        ; a is now: y5 y4 y3 0  0  0  0  0
+    add a, b    ; a is now: y5 y4 y3 x7 x6 x5 x4 x3
+    ld l, a     ; we are done with l now
+    ret
 
 
 ; ------------------------------------------------------------------------------
@@ -110,17 +110,17 @@ calculate_color_cell_pixel_address:
 ;		HL = address of the specified color cell's attribute byte
 ; ------------------------------------------------------------------------------
 calculate_color_cell_attr_address:
-				ld a, c			; a is now: 0  0  0  y7 y6 y5 y4 y3
-				srl a
-				srl a
-				srl a					; a is now: 0  0  0  0  0  0  y7 y6
-				and 0x5b			; a is now: 0  1  0  1  1  0  y7 y6
-				ld h, a			; we are done with h now
-				ld a, c			; a is now: 0  0  0  y7 y6 y5 y4 y3
-				and 0x7				; strip y7 and y6
-				rrca
-				rrca
-				rrca					; a is now: y5 y4 y3 0  0  0  0  0
-				add a, b			; a is now: y5 y4 y3 x7 x6 x5 x4 x3
-				ld l, a			; we are done with l now
-				ret
+		ld a, c     ; a is now: 0  0  0  y7 y6 y5 y4 y3
+		srl a
+		srl a
+		srl a       ; a is now: 0  0  0  0  0  0  y7 y6
+		and 0x5b    ; a is now: 0  1  0  1  1  0  y7 y6
+		ld h, a     ; we are done with h now
+		ld a, c     ; a is now: 0  0  0  y7 y6 y5 y4 y3
+		and 0x7     ; strip y7 and y6
+		rrca
+		rrca
+		rrca        ; a is now: y5 y4 y3 0  0  0  0  0
+		add a, b    ; a is now: y5 y4 y3 x7 x6 x5 x4 x3
+		ld l, a     ; we are done with l now
+		ret
