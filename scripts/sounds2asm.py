27619810b92c9141b0e8048a194ee3d971145a92
Paul Wallace <paulrwall@gmail.com>
2017-01-27 22:08:39 -0800

    Add comments

--- scripts/sounds2asm.py
+++ scripts/sounds2asm.py
@@ -1,6 +1,22 @@
 import sys #system
 import re #regular expressions
 
+###########
+# Script to generate an asm file that when compiled down to tape will use the spectrum beeps to make sound.
+# Input: Single file 
+# 	File format: (arg1, arg2, 3arg)
+# 	arg1 = note the be played: a,b,c,d,e,f,g,a#,c#,d#,e,f#,g
+# 	arg2 = duration in seconds (float | int)
+#	arg3 = how many octaves from the original note. positive numbers go up an octave
+#        while negative numbers go down an octave. I haven't tested how many octaves 
+#		 the spectrum can handle but it's safe to assume that at some point the notes
+#		 will lose accuracy
+# Output: An asm file (name = given_file_name + '.asm')
+# Note: I may improve this script if need be later on. I check for some errors but not all
+#       so be kind to the script :). Feel free to change anything, or just improve the code. 
+#       This is just a quick and dirty script to save some time writing songs for the z80. 
+###########
+
 freqs = {"a"  : (440), 
      "a#" : (466.16), #233.08
      "b"  : (493.88 ), #246.94
