0b782095a8660517fe10f3b426b9f841187135ce
Paul Wallace <paulwallace@Pauls-MacBook-Air.local>
2017-02-13 22:03:16 -0800

    Minor changes

--- src/GameData.asm
+++ src/GameData.asm
@@ -68,35 +68,78 @@ shoe_icon_pixels:
 shoe_icon_attributes:
 
 shoe_sprite_pixels:
-	defb 0,11
-	defb 255,1
-	defb 0,1
+	defb 0,15
 	defb 255,2
 	defb 128,1
-	defb 255,2
 	defb 0,1
+	defb 127,1
+	defb 0,2
 	defb 1,1
 	defb 128,1
-	defb 255,4
 	defb 0,1
-	defb 255,4
+	defb 3,1
+	defb 255,3
+	defb 0,22
+	defb 255,2
+	defb 128,1
 	defb 0,1
-	defb 255,136
-	defb 0,1
-	defb 255,4
+	defb 255,1
 	defb 0,2
-	defb 255,13
-	defb 0,11
+	defb 31,1
+	defb 128,1
+	defb 0,26
+	defb 31,1
+	defb 255,2
+	defb 128,1
+	defb 0,1
+	defb 255,1
+	defb 0,2
+	defb 255,1
+	defb 128,1
+	defb 0,26
+	defb 255,3
+	defb 128,1
+	defb 0,1
+	defb 255,4
+	defb 128,1
+	defb 0,25
 	defb 3,1
 	defb 255,3
 	defb 128,1
-	defb 255,4
-	defb 128,1
-	defb 255,2
-	defb 0,3
+	defb 0,1
+	defb 127,1
 	defb 255,3
-	defb 0,2
-	defb 255,70
+	defb 128,1
+	defb 0,25
+	defb 15,1
+	defb 255,2
+	defb 0,1
+	defb 128,1
+	defb 0,1
+	defb 127,1
+	defb 255,3
+	defb 128,1
+	defb 0,25
+	defb 31,1
+	defb 255,2
+	defb 0,1
+	defb 128,1
+	defb 0,1
+	defb 63,1
+	defb 255,3
+	defb 128,1
+	defb 0,21
+	defb 31,1
+	defb 0,3
+	defb 63,1
+	defb 255,2
+	defb 0,1
+	defb 128,1
+	defb 0,1
+	defb 31,1
+	defb 255,3
+	defb 128,1
+	defb 0,12
 
 
 shoe_sprite_attributes:
@@ -108,23 +151,23 @@ shoe_sprite_attributes:
 ; 	defb 63,7
 ; 	defb 63,1
 ;----------------
-; 	defb 71,6
-; 	defb 71,3
-; 	defb 4,1
-; 	defb 71,1
-; 	defb 4,1
-; 	defb 71,1
-; 	defb 4,3
-; 	defb 39,1
-; 	defb 4,1
-; 	defb 71,1
-; 	defb 2,1
-; 	defb 34,3
-; 	defb 2,1
-; 	defb 71,1
-; 	defb 2,4
-; 	defb 71,1
-; 	defb 71,6
+	defb 71,6
+	defb 71,3
+	defb 4,1
+	defb 71,1
+	defb 4,1
+	defb 71,1
+	defb 4,3
+	defb 39,1
+	defb 4,1
+	defb 71,1
+	defb 2,1
+	defb 34,3
+	defb 2,1
+	defb 71,1
+	defb 2,4
+	defb 71,1
+	defb 71,6
 
 lambtron_data:
 lambtron_name:
