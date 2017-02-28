4155f75c3917182dd2e1c04afbef56f47caf262b
Paul Wallace <paulrwall@gmail.com>
2017-02-27 18:13:43 -0800

    Move character movement constant at 25 FPS and allow holding down of keys to continuously move sprite

--- src/CharacterMove.asm
+++ src/CharacterMove.asm
@@ -1,75 +1,77 @@
+
+; ------------------------------------------------------------------------------
+; Currently characters move at 25 fps. We can make them move slower by keeping a 
+; pointer to what movements are waiting to be processed and decrement those pointers
+; until they reach 0. I don't know how this would play out or if it would be good though. 
+; ------------------------------------------------------------------------------
+
 play_loop: 
       ld bc,65022           ; read keys G,F,D,S,A
       in a,(c)
       ld (character_select_input_store),a ; store input for later
       and 0x1               ; check key A
-      jp nz,__character_select_loop_a_release
-      ld a,1
-      ld hl,a_down
-      ld (hl),a             ; a_down = 1
-      jp __character_select_loop_a_done
-      __character_select_loop_a_release:
-      ld hl,a_down
-      ld a,(hl)
-      or a                  ; if a_down was 1 (key release), select prev character
-      call nz, move_sprite_left
-      ld hl,a_down
-      ld a,0
-      ld (hl),a             ; a_down = 0
-      __character_select_loop_a_done:
-
+      jp nz,__character_select_loop_a_done
+      call halt_2             ; make sure movement is constant and ensure to flickering
+      call move_sprite_left
+;       halt
+__character_select_loop_a_done:
       ld a,(character_select_input_store)
       and 0x4               ; check key D
-      jp nz,__character_select_loop_d_release
-      ld a,1
-      ld hl,d_down
-      ld (hl),a             ; d_down = 1
-      jp __character_select_loop_d_done
-      __character_select_loop_d_release:
-      ld hl,d_down
-      ld a,(hl)
-      or a                  ; if d_down was 1 (key release), select next character
-      call nz, move_sprite_right
-      ld hl,d_down
-      ld a,0
-      ld (hl),a             ; d_down = 0
-      __character_select_loop_d_done:
+      jp nz,__character_select_loop_d_done
+      call halt_2             ; make sure movement is constant and ensure to flickering
+      call move_sprite_right
+
+__character_select_loop_d_done:
 
 
       ld a,(character_select_input_store)
       and 0x2               ; check key S
-      jp nz,__character_select_loop_s_release
-      ld a,1
-      ld hl,s_down
-      ld (hl),a             ; s_down = 1
-      jp __character_select_loop_s_done
-      __character_select_loop_s_release:
-      ld hl,s_down
-      ld a,(hl)
-      or a                  ; if d_down was 1 (key release), select next character
-      call nz, move_sprite_down
-      ld hl,s_down
-      ld a,0
-      ld (hl),a             ; d_down = 0
-      __character_select_loop_s_done:
+      jp nz,__character_select_loop_s_done
+      call halt_2             ; make sure movement is constant and ensure to flickering
+      call move_sprite_down
 
+__character_select_loop_s_done:
       ld bc,64510           ; read keys T, R, E, W, Q
       in a,(c)
       ld (character_select_input_store),a ; save input for later
       ld a,(character_select_input_store)
       and 0x2               ; check key W?????
-      jp nz,__character_select_loop_w_release
-      ld a,1
-      ld hl,w_down
-      ld (hl),a             ; w_down = 1
-      jp __character_select_loop_w_done
-      __character_select_loop_w_release:
-      ld hl,w_down
-      ld a,(hl)
-      or a                  ; if w_down was 1 (key release), select next character
-      call nz, move_sprite_up
-      ld hl,w_down
-      ld a,0
-      ld (hl),a             ; w_down = 0
-      __character_select_loop_w_done:
+      jp nz,__character_select_loop_w_done
+      call halt_2             ; make sure movement is constant and ensure to flickering
+      call move_sprite_up
+__character_select_loop_w_done:
       jp play_loop
+
+
+
+; ------------------------------------------------------------------------------
+; Routine that will check the last time an interrupt was handled by the ROM. 
+; If the last update (interrupt) was not 2 or more cycles ago then this method waits
+; until then. This is to guarentee 25 cycles per second of processing (assuming that no
+; processing takes more than 2 cycles in which case we need to re-evaluate). 
+; Inputs:
+;   A  = Index of the character whose data address should be loaded
+; Outputs:
+;   IX = Address of the data of the character
+; ------------------------------------------------------------------------------
+halt_2:
+      ld hl, last_update
+      ld a, (23672)       ; current timer updated by ROM interrupt routine
+      sub (hl)
+      cp 2
+      jr nc, continue
+      jp halt_2
+continue: 
+      ld a, (23672)
+      ld (hl), a    ;Set the last upate time 
+      ret
+
+
+; ------------------------------------------------------------------------------
+; Stores the time of the last interrupt update by ROM
+; ------------------------------------------------------------------------------
+last_update: defb 0
+
+;       ld a,1
+;       ld hl,a_down
+;       ld (hl),a             ; d_down = 1
