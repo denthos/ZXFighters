b4ca1e15b4c21fa85be429e5f49fece5dcdf5bb2
Dylan Pereira <dpereira@ucsd.edu>
2017-03-18 21:27:43 -0700

    updated character init script

--- utils/CharacterInitCodeWriter.py
+++ utils/CharacterInitCodeWriter.py
@@ -11,19 +11,19 @@ def print_extra():
 	print "        ld (hl),b"
 	print "        inc hl"
 
-def attack_skip():
-	print "        inc hl"
-	print "        inc hl"
+def print_extra2():
+	print "        ld (hl),a"
 	print "        inc hl"
 
-def attack_skip2():
-	print "        inc hl"
+def attack_skip():
 	print "        inc hl"
 	print "        inc hl"
 	print "        inc hl"
 
 name = sys.argv[1]
 print name + "_init:"
+print "        ld bc," + name + "_name"
+print_extra()
 print "        ld bc," + name + "_sprite_idle_1"
 print_extra()
 print "        ld bc," + name + "_sprite_idle_2"
@@ -36,28 +36,45 @@ print "        ld bc," + name + "_sprite_block"
 print_extra()
 print "        ld bc," + name + "_sprite_hit"
 print_extra()
-
-
-attack_skip()
+print "        ld a,(" + name + "_attack_1_starting_active_frame)"
+print_extra2()
+print "        ld a,(" + name + "_attack_1_ending_active_frame)"
+print_extra2()
+print "        ld a,(" + name + "_attack_1_total_frames)"
+print_extra2()
 print "        ld bc," + name + "_sprite_attack_1_1"
 print_extra()
 print "        ld bc," + name + "_sprite_attack_1_2"
 print_extra()
-attack_skip2()
-
-
+print "        inc hl"
+print "        ld bc," + name + "_attack_1_execute"
+print_extra()
 attack_skip()
+print "        ld a,(" + name + "_attack_2_starting_active_frame)"
+print_extra2()
+print "        ld a,(" + name + "_attack_2_ending_active_frame)"
+print_extra2()
+print "        ld a,(" + name + "_attack_2_total_frames)"
+print_extra2()
 print "        ld bc," + name + "_sprite_attack_2_1"
 print_extra()
 print "        ld bc," + name + "_sprite_attack_2_2"
 print_extra()
-attack_skip2()
-
-
+print "        inc hl"
+print "        ld bc," + name + "_attack_2_execute"
+print_extra()
 attack_skip()
+print "        ld a,(" + name + "_attack_3_starting_active_frame)"
+print_extra2()
+print "        ld a,(" + name + "_attack_3_ending_active_frame)"
+print_extra2()
+print "        ld a,(" + name + "_attack_3_total_frames)"
+print_extra2()
 print "        ld bc," + name + "_sprite_attack_3_1"
 print_extra()
 print "        ld bc," + name + "_sprite_attack_3_2"
 print_extra()
-
+print "        inc hl"
+print "        ld bc," + name + "_attack_3_execute"
+print_extra()
 print "        ret"
